e8f6a6079b7acb030c84452fd45e85391120d3f3
MindbniM <1771392560@qq.com>
2025-01-11 13:09:48 +0000

    增加公共工具类

--- code/util.h
+++ code/util.h
@@ -2,13 +2,24 @@
 #include <cxxabi.h>
 namespace MindbniM
 {
-    /**
-     * @brief 将类型名转换为人可以读的简易名称
-     */
-    template<class T>
-    const char* TypeToName()
+    namespace Util
     {
-        static const char* s_name=abi::__cxa_demangle(typeid(T).name(),nullptr,nullptr,nullptr);
-        return s_name;
+        /**
+         * @brief 将类型名转换为人可以读的简易名称
+         */
+        template<class T>
+        const char* TypeToName()
+        {
+            static const char* s_name=abi::__cxa_demangle(typeid(T).name(),nullptr,nullptr,nullptr);
+            return s_name;
+        }
+
+        bool isValidName(const std::string& name) 
+        {
+            auto it=name.find_first_not_of("abcdefghikjlmnopqrstuvwxyz._0123456789");
+            if(it!=std::string::npos)  return false;
+            return true;
+        }
+
     }
 }
